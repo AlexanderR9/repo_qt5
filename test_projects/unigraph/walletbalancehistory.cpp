87252477ae0186ce3a486f0512ddf4532f3c06a7
roman <kosha9@mail.ru>
2025-04-29 16:22:46 +0300

    fix compare token balances in walletbalancehistory

--- test_projects/unigraph/walletbalancehistory.cpp
+++ test_projects/unigraph/walletbalancehistory.cpp
@@ -40,15 +40,23 @@ void WalletBalanceHistory::loadHistoryFile()
     {
         qDebug()<<QString("loadHistoryFile fline[%1]").arg(line);
         SnapshotPointAsset rec;
-        rec.fromFileLine(line);
-        if (!rec.invalid()) {initLastRecord(rec);}
+        rec.fromFileLine(line); //парсим текущую строку файла
+        if (!rec.invalid()) {initLastRecord(rec);} //если строка валидна, то оцениваем в текущим списком m_lastSnapshots
         else qWarning()<<QString("WalletBalanceHistory::loadHistoryFile WARNING: can't read file record [%1]").arg(line);
     }
 }
 void WalletBalanceHistory::initLastRecord(const SnapshotPointAsset &rec)
 {
-    if (m_lastSnapshots.isEmpty()) {m_lastSnapshots.append(rec); return;}
+    //if (m_lastSnapshots.isEmpty()) {m_lastSnapshots.append(rec); return;}
+    int i_asset = findAssetRec(rec.token_address);
+    if (i_asset < 0)  {m_lastSnapshots.append(rec); return;} //нет записи с таким адресом
 
+    if (rec.time > m_lastSnapshots.at(i_asset).time) //запись более свежая, нужно ее заменить
+    {
+        m_lastSnapshots[i_asset].setData(rec);
+    }
+
+    /*
     int n = m_lastSnapshots.count();
     int need_replace = -1;
     bool not_found = true;
@@ -68,6 +76,7 @@ void WalletBalanceHistory::initLastRecord(const SnapshotPointAsset &rec)
         m_lastSnapshots.removeAt(need_replace);
         m_lastSnapshots.append(rec);
     }
+    */
 }
 void WalletBalanceHistory::updateBalances(const QMap<QString, float> &map)
 {
@@ -75,17 +84,23 @@ void WalletBalanceHistory::updateBalances(const QMap<QString, float> &map)
 
     if (map.isEmpty()) return;
     QStringList addrs(map.keys()); //token addresses by wallet
-    QDateTime dt = QDateTime::currentDateTime();
+    //QDateTime dt = QDateTime::currentDateTime();
 
     //записи для токенов в которых были обнаружены изменения баланса более чем на 0.01%
     //соответственно их нужно добавить в файл истории.
     QList<SnapshotPointAsset> new_records;
     foreach (const QString &addr, addrs)
     {
+        float cur_balance = map.value(addr); //new received balance value
+        compareTokenBalances(addr, cur_balance, new_records);
+
+/*
+
         float last_balance = lastAssetBalance(addr);
-        float cur_balance = map.value(addr);
+        qDebug()<<QString("   %1: last_balance=%2  new_balance=%3").arg(addr).arg(last_balance).arg(cur_balance);
         if (last_balance < 0) //not found addr in m_lastSnapshots
         {
+            qDebug("last_balance < 0,  rec not found");
             SnapshotPointAsset next_point(dt, addr, cur_balance);
             new_records.append(next_point);
             m_lastSnapshots.append(next_point);
@@ -94,8 +109,10 @@ void WalletBalanceHistory::updateBalances(const QMap<QString, float> &map)
         {
             float d = qAbs(last_balance - cur_balance);
             float p01 = last_balance/float(10000);
-            if (d > p01)
+            qDebug()<<QString("d_balance=%1,  0.01% = %2").arg(d).arg(p01);
+            if (d > p01) //need update last value
             {
+                qDebug("      d > p01,  need update last value");
                 SnapshotPointAsset next_point(dt, addr, cur_balance);
                 new_records.append(next_point);
                 int pos = findAssetRec(addr);
@@ -103,12 +120,38 @@ void WalletBalanceHistory::updateBalances(const QMap<QString, float> &map)
                 m_lastSnapshots.append(next_point);
             }
         }
+        */
     }
 
     qDebug()<<QString("WalletBalanceHistory::updateBalances  new records %1").arg(new_records.count());
     out();
 
     appendFreshPointToFile(new_records);
+}
+void WalletBalanceHistory::compareTokenBalances(const QString &t_addr, float cur_balance, QList<SnapshotPointAsset> &new_records)
+{
+    int i_asset = findAssetRec(t_addr);
+    if (i_asset < 0)  //нет записи с таким адресом
+    {
+        SnapshotPointAsset next_point(QDateTime::currentDateTime(), t_addr, cur_balance);
+        new_records.append(next_point);
+        m_lastSnapshots.append(next_point);
+        return;
+    }
+
+    //compare
+    float last_balance = m_lastSnapshots.at(i_asset).balance;
+    qDebug()<<QString("   %1: last_balance=%2  new_balance=%3").arg(t_addr).arg(last_balance).arg(cur_balance);
+    float d = qAbs(last_balance - cur_balance);
+    float p01 = last_balance/float(10000);
+    qDebug()<<QString("d_balance=%1,  0.01% = %2").arg(d).arg(p01);
+    if (d > p01) //need update last value
+    {
+        qDebug("      d > p01,  need update last value");
+        SnapshotPointAsset next_point(QDateTime::currentDateTime(), t_addr, cur_balance);
+        new_records.append(next_point);
+        m_lastSnapshots[i_asset].setData(next_point);
+    }
 }
 float WalletBalanceHistory::lastAssetBalance(const QString &t_addr) const
 {
@@ -176,6 +219,12 @@ bool WalletBalanceHistory::SnapshotPointAsset::invalid() const
     if (!time.isValid() || balance < 0) return true;
     if (token_address.length()<30) return true;
     return false;
+}
+void WalletBalanceHistory::SnapshotPointAsset::setData(const SnapshotPointAsset &other)
+{
+    time = other.time;
+    token_address = other.token_address;
+    balance = other.balance;
 }
 QString WalletBalanceHistory::SnapshotPointAsset::toFileLine() const
 {
--- test_projects/unigraph/walletbalancehistory.h
+++ test_projects/unigraph/walletbalancehistory.h
@@ -15,14 +15,19 @@ public:
     {
         SnapshotPointAsset() {reset();}
         SnapshotPointAsset(QDateTime dt, const QString &addr, float b) :time(dt),token_address(addr),balance(b) {}
+
+        //vars
         QDateTime time;
         QString token_address;
         float balance;
+
+        //funcs
         void reset() {time = QDateTime(); token_address.clear(); balance = -1;}
         void fromFileLine(const QString&);
         QString toFileLine() const;
         bool invalid() const;
         QString toStr() const;
+        void setData(const SnapshotPointAsset&);
 
         static QString timeMask() {return QString("yyyy_MM_dd.hh:mm:ss");}
     };
@@ -31,7 +36,10 @@ public:
     WalletBalanceHistory(QObject *parent = NULL);
     virtual ~WalletBalanceHistory() {}
 
-    //каждый раз когда происходит запрос балансов кошелька из сети, нужно затем выполнить эту функцию
+    //каждый раз когда происходит запрос балансов кошелька из сети, нужно затем выполнить эту функцию.
+    //здесь происходит сравнение текущих реальных балансов полученных только что, с теми что в m_lastSnapshots.
+    //если будут замечены отличия в величинах более чем на 0,01% то соответствующие записи будут замененны в m_lastSnapshots,
+    //а так же они будут добавлены в файл истории JS_BALANCE_FILE c текущей датой.
     void updateBalances(const QMap<QString, float>&);
 
 protected:
@@ -43,7 +51,10 @@ protected:
     //m_lastSnapshots заполняется самыми свежими значениями.
     //одна запись в файле имеет вид : date.time / token_address / balance_value
     void loadHistoryFile();
-    void initLastRecord(const SnapshotPointAsset&); //проверить если эта запись более свежая, то заменить ее в m_lastSnapshots на соответствующую
+
+    //проверить если эта запись более свежая, то заменить ее в m_lastSnapshots на соответствующую.
+    //здесь проверяются именно даты а не значения балансов.
+    void initLastRecord(const SnapshotPointAsset&);
 
     //вернет последнее значение баланса для указанного актива(взятое из m_lastSnapshots) или -1
     float lastAssetBalance(const QString&) const;
@@ -51,8 +62,12 @@ protected:
     //вернет индекс в m_lastSnapshots для указанного актива или -1
     int findAssetRec(const QString&) const;
 
-    //добавить обнаруженные более свежие точки балансов
+    //добавить обнаруженные более свежие точки балансов в файл истории JS_BALANCE_FILE c текущей датой.
     void appendFreshPointToFile(const QList<SnapshotPointAsset>&);
+
+    //сравнить текущий баланс указанного токен с тем что лежит в m_lastSnapshots.
+    //если есть отличие, то обновить соответствующую запись в m_lastSnapshots и добавить ее в контейнер для последующего добавления в файл истории.
+    void compareTokenBalances(const QString&, float, QList<SnapshotPointAsset>&);
 
 private:
     void out(); //diag func
